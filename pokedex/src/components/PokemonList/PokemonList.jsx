aee41fa41c3e0ad32c1c4f9d6cd1451aaffbc2fe
Nikhil <ace.nikchauhan@gmail.com>
2023-09-24 08:14:03 +0530

    cleaned up useStates and used advanced useState

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -3,29 +3,41 @@ import axios from "axios";
 import Pokemon from "../Pokemon/Pokemon";
 
 function PokemonList() {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [pokedexUrl, setPokedexUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon"
-  );
+  // const [pokemonList, setPokemonList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [pokedexUrl, setPokedexUrl] = useState(
+  //   "https://pokeapi.co/api/v2/pokemon"
+  // );
 
-  const [nextUrl, setNextUrl] = useState("");
-  const [prevUrl, setPrevUrl] = useState("");
+  // const [nextUrl, setNextUrl] = useState("");
+  // const [prevUrl, setPrevUrl] = useState("");
+
+  const [pokemonListState, setPokemonListState] = useState({
+    pokemonList: [],
+    isLoading: true,
+    pokedexUrl: "https://pokeapi.co/api/v2/pokemon",
+    nextUrl: "",
+    prevUrl: "",
+  });
 
   async function downloadPokemons() {
-    setIsLoading(true);
-    const response = await axios.get(pokedexUrl);
-    console.log("Response: ", response);
+    // setIsLoading(true);
+    setPokemonListState({ ...pokemonListState, isLoading: true });
+
+    const response = await axios.get(pokemonListState.pokedexUrl);
+
     const pokemonResults = response.data.results;
-    console.log("Pokemon Result: ", pokemonResults);
-    setNextUrl(response.data.next);
-    setPrevUrl(response.data.previous);
+
+    setPokemonListState((state) => ({
+      ...state,
+      nextUrl: response.data.next,
+      prevUrl: response.data.previous,
+    }));
 
     const pokePromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
-    console.log("Pokemon Promise: ", pokePromise);
 
     const pokeData = await axios.all(pokePromise);
-    console.log("Pokemon Data: ", pokeData);
+
     const res = pokeData.map((pokeData) => {
       const pokemon = pokeData.data;
       return {
@@ -35,23 +47,26 @@ function PokemonList() {
         types: pokemon.types,
       };
     });
-    console.log("Res after each pokemon in pokedata: ", res);
-    setPokemonList(res);
-    setIsLoading(false);
+
+    setPokemonListState((state) => ({
+      ...state,
+      pokemonList: res,
+      isLoading: false,
+    }));
   }
 
   useEffect(() => {
     downloadPokemons();
-  }, [pokedexUrl]);
+  }, [pokemonListState.pokedexUrl]);
 
   return (
     <>
       <div className="text-center mt-12 font-bold">
         Pokemon List
         <div className="flex flex-wrap m-0  justify-evenly">
-          {isLoading
+          {pokemonListState.isLoading
             ? "Loading..."
-            : pokemonList.map((p) => (
+            : pokemonListState.pokemonList.map((p) => (
                 <Pokemon
                   id={p.id}
                   name={p.name}
@@ -63,16 +78,26 @@ function PokemonList() {
         <div className="m-2 flex align-middle justify-center">
           <button
             className="border-solid border 2px border-gray-400 m-2 p-2 disabled:bg-slate-600"
-            disabled={prevUrl === null}
-            onClick={() => setPokedexUrl(prevUrl)}
+            disabled={pokemonListState.prevUrl === null}
+            onClick={() => {
+              setPokemonListState({
+                ...pokemonListState,
+                pokedexUrl: pokemonListState.prevUrl,
+              });
+            }}
           >
             {" "}
             Prev
           </button>
           <button
             className="border-solid border 2px border-gray-400 m-2 p-2  disabled:bg-slate-600"
-            disabled={nextUrl === null}
-            onClick={() => setPokedexUrl(nextUrl)}
+            disabled={pokemonListState.nextUrl === null}
+            onClick={() =>
+              setPokemonListState({
+                ...pokemonListState,
+                pokedexUrl: pokemonListState.nextUrl,
+              })
+            }
           >
             {" "}
             Next
